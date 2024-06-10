bd67df819174152f6506184735b2fa2fbeda2b6d
godwin-nj <amadigodwin7@gmail.com>
2024-06-10 17:47:08 +0100

    gitignored

--- WealtherWalkingSkeleton/obj/Debug/net6.0/WealtherWalkingSkeleton.AssemblyInfo.cs
+++ WealtherWalkingSkeleton/obj/Debug/net6.0/WealtherWalkingSkeleton.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WealtherWalkingSkeleton")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25d11bc2f956de9afe5473124d09506660f68cfb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f2788a0ecea8b7a7f33fa2e9accfa665f1fa4b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WealtherWalkingSkeleton")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WealtherWalkingSkeleton")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
